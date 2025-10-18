a48f4c537e1233b38a9db8fe37d9e925434c99f4
abigaelseenoi <abigaelseenoi5@gmail.com>
2025-10-18 10:59:11 +0300

    added files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,21 @@
-import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav>
-      <NavLink
-        to="/"
-        className="nav-link"
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        className="nav-link"
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/login"
-        className="nav-link"
-      >
-        Login
-      </NavLink>
+    <nav style={{ padding: "1rem", background: "#eee" }}>
+      <ul style={{ display: "flex", gap: "1rem", listStyle: "none", margin: 0 }}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </nav>
   );
-};
+}
 
 export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes.js";
-import "./index.css"
-
-const router = createBrowserRouter(routes);
+import routes from "./routes"; // our nested routes
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={createBrowserRouter(routes)} />
+  </React.StrictMode>
+);
